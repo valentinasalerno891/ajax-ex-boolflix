f4a81a92583a57f0a632164ca1968a4cae13f8e7
vale <valemil.vs@gmail.com>
2020-09-06 12:53:41 +0200

    ok immagine poster

--- js/myscript.js
+++ js/myscript.js
@@ -1,37 +1,31 @@
-// Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film.
-// Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// Titolo
-// Titolo Originale
-// Lingua
-// Voto
-
-// Milestone 2:
-// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
-// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
-// Qui un esempio di chiamata per le serie tv:
-// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+// Milestone 3: In questa milestone come prima cosa aggiungiamo la copertina del film o della serie al nostro elenco.
+// Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse.
+// Dovremo prendere quindi l’URL base delle immagini di TMDB:
+// https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare
+// (troviamo tutte le dimensioni possibili a questo link: https://www.themoviedb.org/talk/53c11d4ec3a3684cf4006400) per poi aggiungere la parte finale dell’URL passata dall’API.
 
 $(document).ready(function(){
 
+    var url1 = 'https://api.themoviedb.org/3/search/movie';
+    var url2 = 'https://api.themoviedb.org/3/search/tv';
+
     $('.button').click(function(){
-        var film = $('.ricerca input').val();
-        reset(film);
-        searchFilms(film);
-        searchSeries(film);
+
+        var ricerca = $('.ricerca input').val();
+
+        reset();
+        search(ricerca, url1, 'film');
+        search(ricerca, url2, 'serie tv');
     });
 
     $('.ricerca input').keydown(function(){
-        var film = $('.ricerca input').val();
+        var ricerca = $('.ricerca input').val();
         if (event.which == 13 || event.keyCode == 13) {
-            reset(film);
-            searchFilms(film);
-            searchSeries(film);
+            reset();
+            search(ricerca, url1, 'film');
+            search(ricerca, url2, 'serie tv');
         }
-    });
+    })
 });
 
 
@@ -44,11 +38,11 @@ function reset(dati) {
     $('.container-risultati').empty('');
 }
 
-function searchFilms(data) {
+function search(data, url, type) {
 
     $.ajax(
         {
-            url: 'https://api.themoviedb.org/3/search/movie',
+            url: url,
             method: 'GET',
             data: {
                 api_key: '0f860012106ea3b4f9e200aaaf3e1386',
@@ -57,9 +51,9 @@ function searchFilms(data) {
             },
             success: function(risposta) {
                 if (risposta.total_results > 0) {
-                    getFilms(risposta.results);
+                    getResults(risposta.results, type);
                 } else {
-                    $('.container-risultati').html('Nessun film trovato con ' + data)
+                    $('.container-risultati').html('Nessun ricerca trovata con ' + data)
                 }
             },
             error: function() {
@@ -69,55 +63,25 @@ function searchFilms(data) {
     );
 }
 
-function getFilms(data) {
+function getResults(data, type) {
     var source = $("#film-ricercati").html();
     var template = Handlebars.compile(source);
+
     for (var i = 0; i < data.length; i++) {
-        var context = {
-            title: data[i].title,
-            original_title: data[i].original_title,
-            original_language: flag(data[i].original_language),
-            vote_average: stelle(data[i].vote_average),
-        };
-        var html = template(context);
-        $('.container-risultati').append(html);
-    }
-}
-
-function searchSeries(data) {
-
-    $.ajax(
-        {
-            url: 'https://api.themoviedb.org/3/search/tv',
-            method: 'GET',
-            data: {
-                api_key: '0f860012106ea3b4f9e200aaaf3e1386',
-                language: 'it-IT',
-                query: data
-            },
-            success: function(risposta) {
-                if (risposta.total_results > 0) {
-                    getSeries(risposta.results);
-                } else {
-                    $('.container-risultati').html('Nessuna serie TV trovata con ' + data)
-                }
-            },
-            error: function() {
-                alert('errore');
-            }
+        if (type == 'film') {
+            var titolo = data[i].title;
+            var originale = data[i].original_title;
+        } else if (type == 'serie tv') {
+            var titolo = data[i].name;
+            var originale = data[i].original_name;
         }
-    );
-}
-
-function getSeries(data) {
-    var source = $("#film-ricercati").html();
-    var template = Handlebars.compile(source);
-    for (var i = 0; i < data.length; i++) {
         var context = {
-            original_name: data[i].original_name,
-            original_title: data[i].original_title,
+            tipo: type,
+            title: titolo,
+            original_title: originale,
             original_language: flag(data[i].original_language),
             vote_average: stelle(data[i].vote_average),
+            poster: data[i].poster_path
         };
         var html = template(context);
         $('.container-risultati').append(html);
